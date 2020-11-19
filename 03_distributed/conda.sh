cebff536bb5f1c00f89d8d7fa560031a24e7c89f
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2020-11-19 16:35:01 +0100

    Cleaned-up things of what we actually did

--- 03_distributed/conda.sh
+++ 03_distributed/conda.sh
@@ -7,10 +7,7 @@ if [ "${1}" == "install" ]; then
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
 bash Miniconda3-latest-Linux-x86_64.sh -b -p ${conda_base_path} && \
-${conda_base_path}/bin/conda install -c conda-forge --update-deps --yes jupyterlab dask distributed paramiko dask-jobqueue
-source ${conda_base_path}/etc/profile.d/conda.sh
-conda activate base
-pip install asyncssh
+${conda_base_path}/bin/conda install -c conda-forge --update-deps --yes jupyterlab dask python-graphviz distributed paramiko asyncssh
 
 else
 
@@ -18,3 +15,4 @@ source ${conda_base_path}/etc/profile.d/conda.sh
 conda activate base
 
 fi
+
